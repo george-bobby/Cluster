c2d714f5f2826e98d25b5a7aa40d81ba563e3af6
George Bobby <mailto.georgebobby@gmail.com>
2024-12-25 02:56:53 +0530

    cors no security

--- server/index.js
+++ server/index.js
@@ -24,14 +24,15 @@ const PORT = process.env.PORT || 8800;
 dbConnection();
 
 app.use(helmet());
-app.use(
-  cors({
-    origin: "https://cluster-client.vercel.app/",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: false,
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: "https://cluster-client.vercel.app/",
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: false,
+//   })
+// );
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
